d29a63b94670bf513c8b5f4621d29defd23379a2
Anđrêy Bolkonxki <tuananhdaucong@gmail.com>
2023-05-02 23:51:59 +0300

    r1

---
+++ C.java
@@ -0,0 +1,34 @@
+public class C implements H, F {
+
+    private byte k = 1;
+
+    private int g = 1;
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public int cc() {
+        return 42;
+    }
+
+    public float ff() {
+        return 3.14;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+
+    public Object pp() {
+        return this;
+    }
+
+    public double ee() {
+        return 0.000001;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+}
---
+++ G.java
@@ -0,0 +1,26 @@
+public class G implements H {
+
+    private byte d = 1;
+
+    private String e = "hello";
+
+    public byte oo() {
+        return 1;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.HashSet<Integer>;
+    }
+
+    public float ff() {
+        return 0;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.ArrayList<String>();
+    }
+
+    public void ab() {
+        return;
+    }
+}
